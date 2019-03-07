5ddd88c40d75cdacabc90035938db33cc982ccac
ShaikAbdulArafat <shaikabdularafat@gmail.com>
2019-03-07 07:08:38 +0530

    latest update with Pattern33

--- src/pattern/symbol/Pattern33.java
+++ src/pattern/symbol/Pattern33.java
@@ -1,8 +1,15 @@
 package pattern.symbol;
 
 public class Pattern33 {
+/*	
+	ABCDE
+	 ABCD
+	  ABC
+	   AB
+	    A
 
-	public static void main(String[] args) {
+*/
+	/*public static void main(String[] args) {
 		int n = 5;
 		for (int i = n; i >= 1; i--) {
 			for (int j = n - 1; j >= i; j--) {
@@ -13,6 +20,22 @@ public class Pattern33 {
 			}
 			System.out.println();
 		}
-	}
+	}*/
 
+	//Or
+	
+	public static void main(String[] args) {
+		int n=5;
+		int m=5;
+		for(int i = 1; i <= n ; i++){
+			for(int j = i-1; j >= 1; j--){
+				System.out.print(" ");
+			}
+			for(int k = 1;k <= m; k++){
+				System.out.print((char)(64+k));
+			}
+			m--;
+			System.out.println();
+		}
+	}
 }
